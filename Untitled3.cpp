8e4957cb0b22fba8ffa1a24c66b004fe1e9e734c
Nishanta Paudel <nishantapaudel29@gmail.com>
2020-02-04 12:00:15 +0545

    Untitled3.cpp added

---
+++ Untitled3.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+	int A;
+	printf("Enter a number");
+	scanf("%d",&A);
+	if (A%2==0){
+		printf("sumitra antiko chora gunda");
+	}
+		
+		else
+		{ printf("sumitra antiko chori gundini");
+		
+		}
+	
+}
