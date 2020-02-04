07785cc0b3d9a41d5ba72667fa669dcf14b4febd
Nishanta Paudel <nishantapaudel29@gmail.com>
2020-02-04 12:38:07 +0545

    main.c added

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main (){
+	int P;
+	printf("Enter a number");
+	scanf("%d",&P);
+	if (P>=0){
+		printf("The number is positive");
+	}
+	else
+	    {
+		printf("the number is negative");
+	}
+	return 0;
+	    
+}
+
+	
+	
+	
+
+
+
+
